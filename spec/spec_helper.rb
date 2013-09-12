beed5acc2ddeac48fe6a0cda52e0cbe7da0cb1b7
Robbie Marcelo <rbmrclo@hotmail.com>
2013-09-12 22:27:35 +0800

    Setup test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'rspec/rails'
+require 'factories'
+require 'database_cleaner'
 
 require 'glass'
 
@@ -9,13 +12,22 @@ RSpec.configure do |config|
   end
 
   config.include RSpec::Matchers
+  config.include Warden::Test::Helpers
 
   config.before(:each) do
-
+    DatabaseCleaner.start
+    RailsAdmin::Config.reset
+    login_as User.create(
+      :email => "username@example.com",
+      :password => "password"
+    )
   end
 
   config.after(:each) do
-
+    Warden.test_reset!
+    DatabaseCleaner.clean
   end
+
+
 end
 
