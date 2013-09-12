544053f116a7425b236a7057dfaa045c9c066d17
Adrian Peterson Co <adrianpco@gmail.com>
2013-09-12 19:15:04 +0800

    Adds theoretically working api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Glass::Engine.routes.draw do
   controller "main" do
-    get '/',        to: :index,     as: 'glass'
-    get '/config',  to: :config,    as: 'config'
+    get '/',              to: :index,     as: 'glass'
+    get '/glass/config',  to: :config,    as: 'config'
+  end
+
+  controller "api" do
+    get     '/:model_scope',      to: :index,     as: 'index',    via: 'get'
+    get     '/:model_scope/:id',  to: :show,      as: 'show',     via: 'get'
+    post    '/:model_scope',      to: :create,    as: 'create',   via: 'post'
+    put     '/:model_scope/:id',  to: :update,    as: 'update',   via: 'put'
+    delete  '/:model_scope/:id',  to: :delete,    as: 'destroy',  via: 'delete'
   end
 end
