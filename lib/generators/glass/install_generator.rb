9ecd5043353b1507ab048a5d443baab1fbb4bf34
Adrian Peterson Co <adrianpco@gmail.com>
2013-09-12 17:06:03 +0800

    Because I am a bloody idiot

--- lib/generators/glass/install_generator.rb
+++ lib/generators/glass/install_generator.rb
@@ -11,7 +11,7 @@ module Glass
       initializer = (File.open(Rails.root.join('config/initializers/glass.rb')) rescue nil).try :read
 
       gsub_file "config/routes.rb", /mount Glass::Engine => \'api'\, as: :\'rails_admin\'/, ''
-      route("mount Glass::Engine => '/api', as: :glass"
+      route("mount Glass::Engine => '/api', as: :glass")
 
       template "initializer.rb", 'config/initializers/glass.rb' unless initializer
     end
