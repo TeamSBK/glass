a33a45c86e255d6c67628008d5ccc6a661f5d918
Adrian Peterson Co <adrianpco@gmail.com>
2013-09-16 20:20:08 +0800

    Scaffolds tests

--- spec/controllers/glass/main_controller_spec.rb
+++ spec/controllers/glass/main_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Glass::MainController do
+
+  describe "#index" do
+
+  end
+
+  describe "methods" do
+
+    describe "#setup_config" do
+
+    end
+
+  end
+
 end
