265bfbb3dd802398c1056e66b70eee3af9049c70
Robbie Marcelo <rbmrclo@hotmail.com>
2013-09-19 21:33:51 +0800

    :facepunch: DefineORM  :facepunch:

--- app/controllers/glass/main_controller.rb
+++ app/controllers/glass/main_controller.rb
@@ -1,6 +1,7 @@
 module Glass
   class MainController < Glass::ApplicationController
     before_filter :setup_config
+    before_filter :define_orm
 
     def index
       render json: Glass::Config.to_json
@@ -9,6 +10,10 @@ module Glass
   private
     def setup_config
       Glass::Config.setup
+    end
+
+    def define_orm
+      Glass::DefineOrm.new(Glass::Config.models)
     end
 
   end
--- lib/glass/define_orm.rb
+++ lib/glass/define_orm.rb
@@ -1,16 +1,18 @@
 module Glass
-  class AbstractModel
+  class DefineOrm
     attr_accessor :adapter, :model_names
 
     def initialize(models)
       @model_names = models
 
       if models[0].constantize.ancestors.map(&:to_s).include?('ActiveRecord::Base')
+
         # ActiveRecord
         @adapter = :active_record
         Glass::Config.adapter = @adapter
 
       elsif models[0].constantize.ancestors.map(&:to_s).include?('Mongoid::Document')
+
         # Mongoid
         @adapter = :mongoid
         Glass::Config.adapter = @adapter
@@ -24,7 +26,7 @@ module Glass
       model_names.each do |model|
         model.constantize.instance_eval do
           def column_names
-            self.fields.collect { |field| field[0].tr('^A-Za-z0-9', '') }
+            self.fields.collect { |field| field[0].tr('^_A-Za-z0-9', '') }
           end
         end
       end
