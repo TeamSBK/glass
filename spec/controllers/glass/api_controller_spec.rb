996186a0dae8e633100c87710720b3371aa3abe8
Adrian Peterson Co <adrianpco@gmail.com>
2014-02-07 20:36:45 +0800

    Adds pending tests

--- spec/controllers/glass/api_controller_spec.rb
+++ spec/controllers/glass/api_controller_spec.rb
@@ -11,19 +11,28 @@ describe Glass::ApiController, type: :controller do
     end
 
     describe "#show" do
+      it 'pending' do
+        pending
+      end
     end
 
 
     describe "#create" do
-
+      it 'pending' do
+        pending
+      end
     end
 
     describe "#update" do
-
+      it 'pending' do
+        pending
+      end
     end
 
     describe "#destroy" do
-
+      it 'pending' do
+        pending
+      end
     end
 
   end
@@ -31,19 +40,27 @@ describe Glass::ApiController, type: :controller do
   describe "methods" do
 
     describe "#sanitize_model" do
-
+      it 'pending' do
+        pending
+      end
     end
 
     describe "#sanitize_params" do
-
+      it 'pending' do
+        pending
+      end
     end
 
     describe "#validate_model" do
-
+      it 'pending' do
+        pending
+      end
     end
 
     describe "#ignored_keys" do
-
+      it 'pending' do
+        pending
+      end
     end
 
   end
--- spec/controllers/glass/main_controller_spec.rb
+++ spec/controllers/glass/main_controller_spec.rb
@@ -18,6 +18,7 @@ describe Glass::MainController do
   describe "#index" do
 
     it "returns the Glass configuration" do
+      pending
     end
 
   end
